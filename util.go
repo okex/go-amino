5223a7bea08c45428cb3a0f3325e9bbafa3aaa41
cwbhhjl <cwbhhjl@gmail.com>
2021-11-01 15:07:38 +0800

    add util function ParseProtoPosAndTypeMustOneByte

---
+++ util.go
@@ -0,0 +1,12 @@
+package amino
+
+// ParseProtoPosAndTypeMustOneByte Parse feild number and type from one byte,
+// if original feild number and type encode to multiple bytes, you should not use this function.
+func ParseProtoPosAndTypeMustOneByte(data byte) (pos int, aminoType Typ3) {
+	if data&0x80 == 1 {
+		panic("varint more than one byte")
+	}
+	aminoType = Typ3(data & 0x07)
+	pos = int(data) >> 3
+	return
+}
