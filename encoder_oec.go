e4ba1443bb15cdc38ada9214a3e4a68a31259be3
cwbhhjl <cwbhhjl@gmail.com>
2022-01-27 17:06:40 +0800

    update encoder

--- encoder_oec.go
+++ encoder_oec.go
@@ -1,14 +1,9 @@
 package amino
 
-import "bytes"
-
-func EncodeInt8WithKeyToBuffer(w *bytes.Buffer, i int8, key ...byte) (err error) {
-	_, err = w.Write(key)
-	if err != nil {
-		return
-	}
-	return EncodeVarintToBuffer(w, int64(i))
-}
+import (
+	"bytes"
+	"time"
+)
 
 func EncodeByteSliceWithKeyToBuffer(w *bytes.Buffer, bz []byte, key ...byte) error {
 	_, err := w.Write(key)
@@ -42,4 +37,12 @@ func EncodeBoolWithKeyToBuffer(w *bytes.Buffer, b bool, key ...byte) error {
 		return err
 	}
 	return EncodeBoolToBuffer(w, b)
+}
+
+func EncodeTimeWithKeyToBuffer(w *bytes.Buffer, t time.Time, key ...byte) error {
+	_, err := w.Write(key)
+	if err != nil {
+		return err
+	}
+	return EncodeTimeToBuffer(w, t)
 }
